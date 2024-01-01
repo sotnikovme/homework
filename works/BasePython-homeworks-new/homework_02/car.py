617386bf70307ab38c1f2f3975a009b50976c826
sotnikovme <sotnikovme@gmail.com>
2024-01-01 16:36:01 +0300

    hw1

--- works/BasePython-homeworks-new/homework_02/car.py
+++
@@ -1,9 +0,0 @@
-from engine import Engine
-from base import Vehicle
-
-
-class Car(Vehicle):
-    type = Engine
-    def set_engine(self):
-        Engine.volume
-        Engine.pistons
--- works/BasePython-homeworks-new/homework_02/engine.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Engine:
-    volume = 2
-    pistons = 6
-    def __init__(self, volume, pistons):
-        self.volume = volume
-        self.pistons = pistons 
