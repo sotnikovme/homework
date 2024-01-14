710ce300f81b752303cd16e1e02bb4400152a787
sotnikovme <sotnikovme@gmail.com>
2024-01-14 21:13:31 +0300

    04

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -1,16 +1,17 @@
 """
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
-import asyncio
-import logging
+#import asyncio
+#import logging
 import aiohttp
-from aiohttp import ClientSession
-from homework_04.common import configure_logging
+#from aiohttp import ClientSession
+#from homework_04.common import configure_logging
 
-log = logging.getLogger(__name__)
+#log = logging.getLogger(__name__)
 
-USERS_DATA_URL = "https://jsonplaceholder.typicode.com/"
-POSTS_DATA_URL = "https://github.com/typicode/xv.git"
+USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
+POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
+
 
 async def fetch_json(url: str):
     async with aiohttp.ClientSession() as session:
@@ -18,11 +19,20 @@ async def fetch_json(url: str):
             data: dict = await response.json()
             return data
 
-async def main():
-    configure_logging()
-    result = await fetch_json(USERS_DATA_URL)
-    result = await fetch_json(POSTS_DATA_URL)
-    log.info("got result %s", result)
+async def fetch_users():
+    data = fetch_json(USERS_DATA_URL)
+    return data
 
-if  __name__ == "__main__":
-    asyncio.run(main())
+async def fetch_posts():
+    data = fetch_json(USERS_DATA_URL)
+    return data
+
+
+# async def main():
+#     configure_logging()
+#     result = await fetch_json(USERS_DATA_URL)
+#     result = await fetch_json(POSTS_DATA_URL)
+#     log.info("got result %s", result)
+
+#if  __name__ == "__main__":
+#    asyncio.run(main())
