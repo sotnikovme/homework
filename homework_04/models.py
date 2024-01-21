85e645d542cf396db44581ee4e1df08edca85e28
sotnikovme <sotnikovme@gmail.com>
2024-01-21 11:14:43 +0300

    hw4_new

--- homework_04/models.py
+++ homework_04/models.py
@@ -9,8 +9,61 @@
 """
 
 import os
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+)
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import (
+    declarative_base,
+    relationship,
+    sessionmaker,
+)
 
 PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
+engine = create_async_engine(PG_CONN_URI, echo=False)
+Base = declarative_base()
+Session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-Base = None
-Session = None
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, default="", server_default="")
+    username = Column(String, nullable=False, default="", server_default="")
+    email = Column(String, nullable=False, default="", server_default="")
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
+    title = Column(String, nullable=False, default="", server_default="")
+    body = Column(String, nullable=False, default="", server_default="")
+
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+async def fill_users_table(users_data):
+    async with Session() as session:
+        session: AsyncSession
+        async with session.begin():
+            for user_ in users_data:
+                user_added = User(name=user_['name'], username=user_['username'], email=user_['email'])
+                session.add(user_added)
+
+                
+async def fill_posts_table(posts_data):
+    async with Session() as session:
+        session: AsyncSession
+        async with session.begin():
+            for post_ in posts_data:
+                post_added = Post(user_id=post_['userId'], title=post_['title'], body=post_['body'])
+                session.add(post_added)
+
